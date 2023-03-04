3024565f47276bf4c2ff18c4eb47655af9effa85
Jean Silva <jeanbarrossilva@outlook.com>
2023-03-04 16:08:46 -0300

    fix(bug): missing generated Properties.extensions file

--- src/main/kotlin/com/jeanbarrossilva/newandlib/Generator.kt
+++ src/main/kotlin/com/jeanbarrossilva/newandlib/Generator.kt
@@ -23,6 +23,43 @@ internal object Generator {
 
     context(Prompter)
     private fun FileWriter.writeBuildSrcFiles() {
+        writeTo("buildSrc/src/main/java/utils/Properties.extensions", """
+            import java.io.File
+            import java.io.InputStreamReader
+            import java.util.Properties
+            
+            /**
+             * Creates [Properties] according to the `local.properties` file.
+             *
+             * @param root Directory in which the `local.properties` file is.
+             **/
+            fun localProperties(root: File): Properties {
+                val file = File(root, "local.properties")
+                return Properties().apply { tryToLoad(file) }
+            }
+            
+            /**
+             * Loads the given [file] into these [Properties].
+             *
+             * @param file [File] to be loaded.
+             **/
+            private fun Properties.load(file: File) {
+                file.inputStream().reader().use {
+                    load(it)
+                }
+            }
+            
+            /**
+             * Loads the given [file] into these [Properties] if it's a normal file.
+             *
+             * @param file [File] to be loaded.
+             **/
+            private fun Properties.tryToLoad(file: File) {
+                if (file.isFile) {
+                    load(file)
+                }
+            }
+        """)
         writeTo("buildSrc/src/main/java/utils/RepositoryHandler.extensions.kt", """
             import java.net.URI
             import org.gradle.api.Project
@@ -164,7 +201,7 @@ internal object Generator {
     }
 
     context(Prompter)
-            private fun FileWriter.writeRootFiles() {
+    private fun FileWriter.writeRootFiles() {
         writeTo(".gitignore", """
             *.iml
             .gradle
