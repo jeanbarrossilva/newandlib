289d1990d5e660e7689eb26b83e8850d7884c54c
Jean Silva <jeanbarrossilva@outlook.com>
2023-06-10 14:26:35 -0300

    refact: remove remainings of key-based prompts

--- src/main/kotlin/com/jeanbarrossilva/newandlib/Prompts.kt
+++
@@ -1,9 +0,0 @@
-package com.jeanbarrossilva.newandlib
-
-internal object Prompts {
-    const val GROUP_ID = "group-id"
-    const val LIBRARY_MODULE_NAMESPACE = "library-module-namespace"
-    const val PROJECT_NAME = "project-name"
-    const val PROJECT_PATH = "project-path"
-    const val REPOSITORY_URL = "repository-url"
-}
